75bf88a1788f9665d136c9e15de4b4505999a3ca
Job Noorman <job.noorman@cs.kuleuven.be>
2013-01-28 08:34:29 +0100

    Improved messages when finding MSPGCC

--- cmake/FindMSPGCC.cmake
+++ cmake/FindMSPGCC.cmake
@@ -1,7 +1,6 @@
 include(FindPackageHandleStandardArgs)
 
 find_program(MSPGCC_EXECUTABLE msp430-gcc)
-find_package_handle_standard_args(MSPGCC DEFAULT_MSG MSPGCC_EXECUTABLE)
 
 get_filename_component(TMP ${MSPGCC_EXECUTABLE} PATH)
 get_filename_component(MSPGCC_BASE_DIR ${TMP}/../msp430 ABSOLUTE)
@@ -9,10 +8,10 @@ set(TMP)
 set(MSPGCC_LIB_DIR ${MSPGCC_BASE_DIR}/lib)
 set(MSPGCC_INCLUDE_DIR ${MSPGCC_BASE_DIR}/include)
 
-if (EXISTS ${MSPGCC_BASE_DIR} AND
-    EXISTS ${MSPGCC_LIB_DIR} AND
-    EXISTS ${MSPGCC_INCLUDE_DIR})
-    message(STATUS "Found MSPGCC installation directory: ${MSPGCC_BASE_DIR}")
-else ()
-    message(FATAL_ERROR "Cannot find MSPGCC installation directory")
+if (NOT EXISTS ${MSPGCC_BASE_DIR} OR
+    NOT EXISTS ${MSPGCC_LIB_DIR} OR
+    NOT EXISTS ${MSPGCC_INCLUDE_DIR})
+    set(MSPGCC_BASE_DIR)
 endif ()
+
+find_package_handle_standard_args(MSPGCC DEFAULT_MSG MSPGCC_EXECUTABLE MSPGCC_BASE_DIR)
