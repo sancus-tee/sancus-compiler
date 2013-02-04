9fbc47e0e25b9008f64cade66a88a6152dc7cb99
Job Noorman <jobnoorman@gmail.com>
2013-02-04 09:17:23 +0100

    Added better compiler version checks

--- cmake/CompilerConfig.cmake
+++ cmake/CompilerConfig.cmake
@@ -1,1 +1,29 @@
-set(CMAKE_CXX_FLAGS "-Wall -Wextra -fno-rtti -std=c++0x")
+set(GCC_MIN_VERSION "4.7.0")
+set(CLANG_MIN_VERSION "3.0")
+
+macro(get_compiler_version)
+    execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion
+                    OUTPUT_VARIABLE COMPILER_VERSION)
+endmacro()
+
+# check compiler version. needed since I want to use C++11 features :-)
+if (CMAKE_COMPILER_IS_GNUCXX)
+    get_compiler_version()
+
+    if (COMPILER_VERSION VERSION_LESS ${GCC_MIN_VERSION})
+        message(FATAL_ERROR "GCC version >= ${GCC_MIN_VERSION} required")
+    endif()
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    get_compiler_version()
+
+    if (COMPILER_VERSION VERSION_LESS ${CLANG_MIN_VERSION})
+        message(FATAL_ERROR "Clang version >= ${CLANG_MIN_VERSION} required")
+    endif()
+
+    message(WARNING "This project has not been tested when compiled using Clang")
+else ()
+    message(FATAL_ERROR "Only GCC and Clang are supported")
+endif ()
+
+set(CMAKE_CXX_FLAGS "-Wall -Wextra -fno-rtti -std=c++11")
+
