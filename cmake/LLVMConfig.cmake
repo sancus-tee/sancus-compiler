6bea9ff98e4a6424648b94d7ea1116e94de0369b
Job Noorman <job.noorman@cs.kuleuven.be>
2014-02-25 09:11:39 +0100

    Workaround some LLVM cmake issues

--- cmake/LLVMConfig.cmake
+++ cmake/LLVMConfig.cmake
@@ -1,4 +1,6 @@
 find_package(LLVM REQUIRED)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${LLVM_CMAKE_DIR})
+include(HandleLLVMOptions)
 include(AddLLVM)
 add_definitions(${LLVM_DEFINITIONS})
 include_directories(${LLVM_INCLUDE_DIRS})
