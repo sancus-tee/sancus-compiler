2494dc35fd208ced936f445c73dbe092c1ca055b
Job Noorman <job@noorman.info>
2014-07-23 14:41:47 +0200

    Fix compile error with new version of LLVM.

--- src/llvm/SancusModuleCreator.cpp
+++ src/llvm/SancusModuleCreator.cpp
@@ -146,11 +146,12 @@ bool SancusModuleCreator::handleFunction(Function& f)
             }
 
             DebugLoc loc = inst->getDebugLoc();
-            Twine locStr;
+            std::string locStr;
             if (!loc.isUnknown())
             {
-                locStr = " (" + Twine(loc.getLine()) + ":" +
-                         Twine(loc.getCol()) + ")";
+                Twine t(" (" + Twine(loc.getLine()) + ":" +
+                        Twine(loc.getCol()) + ")");
+                locStr = t.str();
             }
 
             errs() << "WARNING: In function " << f.getName() << locStr
