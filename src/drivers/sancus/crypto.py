cb2549ab2984a34b0863073898e2229dd245445f
Job Noorman <job.noorman@cs.kuleuven.be>
2014-10-09 17:28:01 +0200

    Make sure arguments passed to ctypes are bytes, not bytearrays.

--- src/drivers/sancus/crypto.py
+++ src/drivers/sancus/crypto.py
@@ -54,23 +54,26 @@ def _output_data(data):
 
 
 def wrap(key, ad, body):
+    # NOTE ctypes only understands bytes, not bytearrays
     cipher = bytes(len(body))
     tag = bytes(int(KEY_SIZE / 8))
-    ok = _lib.sancus_wrap(key, ad, ctypes.c_ulonglong(len(ad)),
-                          body, ctypes.c_ulonglong(len(body)), cipher, tag)
+    ok = _lib.sancus_wrap(bytes(key), bytes(ad), ctypes.c_ulonglong(len(ad)),
+                          bytes(body), ctypes.c_ulonglong(len(body)),
+                          cipher, tag)
     return (cipher, tag) if ok else None
 
 
 def unwrap(key, ad, cipher, tag):
     body = bytes(len(cipher))
-    ok = _lib.sancus_unwrap(key, ad, ctypes.c_ulonglong(len(ad)),
-                            cipher, ctypes.c_ulonglong(len(cipher)), tag, body)
+    ok = _lib.sancus_unwrap(bytes(key), bytes(ad), ctypes.c_ulonglong(len(ad)),
+                            bytes(cipher), ctypes.c_ulonglong(len(cipher)),
+                            bytes(tag), bytes(body))
     return body if ok else None
 
 
 def mac(key, msg):
     ret = bytes(int(KEY_SIZE / 8))
-    _lib.sancus_mac(key, msg, ctypes.c_ulonglong(len(msg)), ret)
+    _lib.sancus_mac(bytes(key), bytes(msg), ctypes.c_ulonglong(len(msg)), ret)
     return ret
 
 
