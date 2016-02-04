8b01efd5fbb2bedc1cf31697235d0afe9e5eb929
Job Noorman <job.noorman@cs.kuleuven.be>
2016-02-04 17:49:30 +0100

    Make sure all arguments to the I/O macros are properly expanded.

--- src/sancus_support/reactive.h
+++ src/sancus_support/reactive.h
@@ -7,17 +7,20 @@
 
 typedef uint16_t io_index;
 
+#define __EXP(x) x
+
 // The ASM symbols are used for the linker to be able to detect inputs/outputs
 
 #define SM_OUTPUT(sm, name)                                             \
     asm("__sm_" __STR(sm) "_output_tag_" __STR(name) " = 0\n");         \
-    SM_FUNC(sm) void name(const void* data, size_t len)                 \
+    SM_FUNC(sm) void __EXP(name)(const void* data, size_t len)          \
     {                                                                   \
-        extern char __sm_##sm##_io_##name##_idx;                        \
-        SM_FUNC(sm) void __sm_##sm##_send_output(unsigned int,          \
-                                                 const void*, size_t);  \
-        __sm_##sm##_send_output((io_index)&__sm_##sm##_io_##name##_idx, \
-                                data, len);                             \
+        extern char __sm_##__EXP(sm)##_io_##__EXP(name)##_idx;          \
+        SM_FUNC(sm) void __sm_##__EXP(sm)##_send_output(                \
+                                   unsigned int, const void*, size_t);  \
+        __sm_##__EXP(sm)##_send_output(                                 \
+                (io_index)&__sm_##__EXP(sm)##_io_##__EXP(name)##_idx,   \
+                data, len);                                             \
     }
 
 #define SM_INPUT(sm, name, data_name, len_name)                         \
