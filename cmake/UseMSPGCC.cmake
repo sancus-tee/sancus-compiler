eb4dcaf22a649c7bdf4e76dc5be246b3ae4b0410
Hans Winderix <hans.winderix@skynet.be>
2018-12-22 19:42:15 +0100

    Fixed include directory for msp430-elf compilation

--- cmake/UseMSPGCC.cmake
+++ cmake/UseMSPGCC.cmake
@@ -1,4 +1,4 @@
 set(MCU msp430f149)
 set(CMAKE_C_COMPILER ${MSPGCC_EXECUTABLE})
-include_directories (${MSPGCC_INCLUDEDIR})
+include_directories (${MSPGCC_BASE_DIR}/../include)
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmcu=${MCU}")
