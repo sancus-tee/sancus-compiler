05dca4ea6d0b835b4d2c2ecc20a77d3ff0696fc4
Hans Winderix <hans.winderix@skynet.be>
2018-12-29 12:31:09 +0100

    Export MCU variable correctly

--- cmake/UseMSPGCC.cmake
+++ cmake/UseMSPGCC.cmake
@@ -1,4 +1,11 @@
-set(MCU msp430f149)
 set(CMAKE_C_COMPILER ${MSPGCC_EXECUTABLE})
-include_directories (${MSPGCC_BASE_DIR}/../include)
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmcu=${MCU}")
+
+if(MSP430_GCC_PREFIX STREQUAL msp430-elf)
+  set(mcu msp430sancus)
+  include_directories (${MSPGCC_BASE_DIR}/../include)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmcu=${mcu}")
+else()
+  set(mcu msp430f149)
+endif()
+
+string(TOUPPER mcu MCU)
