6152f0e75fc1f5d282d51b90f038fcf0a58528ff
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-10-06 11:45:33 +0200

    __sm_attest: return a value to indicate success/error

--- src/stubs/sm_attest.c
+++ src/stubs/sm_attest.c
@@ -1,11 +1,13 @@
 #include "reactive_stubs_support.h"
 
-void SM_ENTRY(SM_NAME) __sm_attest(const uint8_t* challenge, size_t len, uint8_t *result)
+uint16_t SM_ENTRY(SM_NAME) __sm_attest(const uint8_t* challenge, size_t len,
+  uint8_t *result)
 {
     if( !sancus_is_outside_sm(SM_NAME, (void *) challenge, len) ||
         !sancus_is_outside_sm(SM_NAME, (void *) result, SANCUS_TAG_SIZE) ) {
-      return;
+      return 1;
     }
 
     sancus_tag(challenge, len, result);
+    return 0;
 }
